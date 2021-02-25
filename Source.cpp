fae15026d95f62ef019f18effc85f8683b17c977
breezewinter <76595288+breezewinter@users.noreply.github.com>
2021-02-25 17:46:29 -0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,37 @@
+#include "functions.h"
+/*
+#define wEntityList = 0x4DA2F44;
+#define forcejump = 0x524CEA4;
+#define m_fFlags = 0x104;
+#define m_iHealth = 0x100;
+*/
+
+int main()
+{
+	
+	system("color 71");
+	coolLoading();
+	hwnd = FindWindowA(NULL, "Counter-Strike: Global Offensive");
+	GetWindowThreadProcessId(hwnd, & procId);
+	moduleBase = GetModuleBaseAddress("client.dll");
+	hProc = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
+	uintptr_t forceJumpValue = 0;
+	printf("Cheat Loaded!\n");
+		
+	while (true)
+	{
+		int flags = RPM<int>(localPlayer + 0x104); // localplayer + m_fflags
+		if (flags == 255)
+		{
+			forceJumpValue = 5;
+		}
+		else
+		{
+			forceJumpValue = 4;
+		}
+		if (GetAsyncKeyState(VK_SPACE))
+		{
+			WPM<uintptr_t>(moduleBase + 0x524CEA4, forceJumpValue);
+		}
+	}
+}
---
+++ functions.h
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+uintptr_t moduleBase;
+DWORD procId;
+HWND hwnd;
+HANDLE hProc;
+
+uintptr_t GetModuleBaseAddress(const char* modName) {
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+	if (hSnap != INVALID_HANDLE_VALUE) {
+		MODULEENTRY32 modEntry;
+		modEntry.dwSize = sizeof(modEntry);
+		if (Module32First(hSnap, &modEntry)) {
+			do {
+				if (!strcmp(modEntry.szModule, modName)) {
+					CloseHandle(hSnap);
+					return (uintptr_t)modEntry.modBaseAddr;
+				}
+			} while (Module32Next(hSnap, &modEntry));
+		}
+	}
+	return 0;
+}
+
+template<typename T> T RPM(SIZE_T address) {
+	T newValue;
+	ReadProcessMemory(hProc, (LPCVOID)address, &newValue, sizeof(T), NULL);
+	return newValue;
+}
+
+template<typename T> void WPM(SIZE_T address, T newValue) {
+	WriteProcessMemory(hProc, (LPVOID)address, &newValue, sizeof(newValue), NULL);
+}
+
+uintptr_t localPlayer = moduleBase + 0xD8B2BC;
+
+bool healthCheck()
+{
+	int health = RPM<int>(moduleBase + 0x100);
+	if (health < 1)
+	{
+		return false;
+	}
+	if (health > 1)
+	{
+		return true;
+	}
+}
+
+int i = 0;
+
+void coolLoading()
+{
+	while (i++ < 4)
+	{
+		std::ostream& o = (std::cout << "Loading...\n");
+		Sleep(500);
+	}
+}
